924006c4e520600bec22dc0b7505e3083ad1b10a
Desmond <desmondmpofu@gmail.com>
2024-07-05 15:54:28 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react'
 import logoImg from '../assets/logo.jpg';
 import Button from './ui/Button'
+import CartContext from '../store/CartContext'
 export default function Header(){
+   const cartCtx = useContext(CartContext)
+   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=>{
+    return totalNumberOfItems+item.quantity
+   }, 5);
     return(
         <header id="main-header">
             <div id="title">
@@ -8,7 +14,7 @@ export default function Header(){
                 <h1>Simbonge's Foods</h1>
             </div>
             <nav>
-                <Button textOnly> Cart (0)</Button>
+                <Button textOnly> Cart ({totalCartItems})</Button>
             </nav>
             </header>
 
